253b289111d19a5c0f02876ac389411064424829
System Administrator <root@SJH-706-25.local>
2025-01-22 10:18:49 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { interval } from 'rxjs';
 import { map , take } from 'rxjs/operators';
+import { MyserviceService } from './myservice.service';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NewCmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -31,7 +33,18 @@ export class AppComponent {
 
   decimalNum1: number = 8.7589623;
   decimalNum2: number = 5.43;
+
+  todaydate;
+  componentproperty;
+  constructor(private myservice:MyserviceService){
+    this.todaydate = this.myservice.showTodayDate()
+    this.componentproperty = this.myservice.serviceproperty;
+    
+  }
+
 }
+
+
 
 
 
